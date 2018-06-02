fd947f718323781de9c682edae4a672c59257dad
Kevin <kevinqiyf@hotmail.com>
2018-06-01 22:08:03 -0700

    Added problem #6 solutions

--- separatePositive.js
+++ separatePositive.js
@@ -1,7 +1,6 @@
 // Write a function called separatePositive which accepts an array of non-zero integers. Separate the positive integers to the left and the negative integers to the right. The positive numbers and negative numbers need not be in sorted order.
 
 // Examples:
-
 // separatePositive([2, -1, -3, 6, -8, 10]) // [2, 10, 6, -3, -1, -8]
 // separatePositive([5, 10, -15, 20, 25]) // [5, 10, 25, 20, -15]
 // separatePositive([-5, 5]) // [5, -5]
