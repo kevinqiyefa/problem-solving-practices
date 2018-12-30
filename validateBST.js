f59b95bce1d270c16dadf9ba5d047b9f3b20a6a6
Kevin <kevinqiyf@hotmail.com>
2018-12-29 19:56:23 -0800

    validate binary search tree

---
+++ validateBST.js
@@ -0,0 +1,64 @@
+// Given a binary tree, determine if it is a valid binary search tree (BST).
+
+// Assume a BST is defined as follows:
+
+// The left subtree of a node contains only nodes with keys less than the node's key.
+// The right subtree of a node contains only nodes with keys greater than the node's key.
+// Both the left and right subtrees must also be binary search trees.
+// Example 1:
+
+// Input:
+//     2
+//    / \
+//   1   3
+// Output: true
+// Example 2:
+
+//     5
+//    / \
+//   1   4
+//      / \
+//     3   6
+// Output: false
+// Explanation: The input is: [5,1,4,null,null,3,6]. The root node's value
+//              is 5 but its right child's value is 4.
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(data) {
+    if (data < this.data && this.left) {
+      this.left.insert(data);
+    } else if (data < this.data) {
+      this.left = new Node(data);
+    } else if (data > this.data && this.right) {
+      this.right.insert(data);
+    } else if (data > this.data) {
+      this.right = new Node(data);
+    }
+  }
+}
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+  function helper(rt, min, max) {
+    if (!rt) return true;
+    if (min >= rt.val || max <= rt.val) return false;
+    return helper(rt.left, min, rt.val) && helper(rt.left, rt.val, max);
+  }
+
+  return helper(root, -Infinity, Infinity);
+};
