b7618c91485a697365a21b1dacbcb9f9469fa547
Kevin <kevinqiyf@hotmail.com>
2019-01-09 18:41:13 -0800

    Group Anagrams

---
+++ GroupAnagrams.js
@@ -0,0 +1,54 @@
+// 49. Group Anagrams
+
+// Given an array of strings, group anagrams together.
+
+// Example:
+
+// Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+// Output:
+// [
+//   ["ate","eat","tea"],
+//   ["nat","tan"],
+//   ["bat"]
+// ]
+// Note:
+
+// All inputs will be in lowercase.
+// The order of your output does not matter.
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+  //sorting
+  //      let hash = {};
+
+  //     for(var i = 0; i < strs.length; i++) {
+  //         let str = strs[i];
+  //         let key = Array.from(str).sort().join('')
+
+  //         hash[key] = hash[key] || [];
+  //         hash[key].push(str);
+  //     }
+
+  //     return Object.values(hash);
+
+  //counting
+  let hash = {};
+  let base = 'a'.charCodeAt(0);
+
+  for (let i = 0; i < strs.length; i++) {
+    let arr = Array(26).fill(0);
+    for (let j = 0; j < strs[i].length; j++) {
+      var code = strs[i][j].charCodeAt(0) - base;
+      arr[code]++;
+    }
+
+    let key = arr.join('');
+    hash[key] = hash[key] || [];
+    hash[key].push(strs[i]);
+  }
+
+  return Object.values(hash);
+};
