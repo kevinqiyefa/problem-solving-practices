962652d42839e97eb305204bbcc9cbc1f5cb17b2
Kevin <kevinqiyf@hotmail.com>
2019-01-26 21:43:38 -0800

    Search in Rotated Sorted Array

---
+++ SearchinRotatedSortedArray.js
@@ -0,0 +1,52 @@
+// 33. Search in Rotated Sorted Array
+
+// Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+// (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+// You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+// You may assume no duplicate exists in the array.
+
+// Your algorithm's runtime complexity must be in the order of O(log n).
+
+// Example 1:
+
+// Input: nums = [4,5,6,7,0,1,2], target = 0
+// Output: 4
+// Example 2:
+
+// Input: nums = [4,5,6,7,0,1,2], target = 3
+// Output: -1
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = ((left + right) / 2) >> 0;
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target <= nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      if (nums[mid] <= target && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+
+  return -1;
+};
