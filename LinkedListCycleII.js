9c143e5af425cdb60607e088d4c93eec6fb6878e
Kevin <kevinqiyf@hotmail.com>
2019-10-29 17:05:17 -0700

    add more

---
+++ LinkedListCycleII.js
@@ -0,0 +1,65 @@
+// 142. Linked List Cycle II
+// Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+// To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+
+// Note: Do not modify the linked list.
+
+// Example 1:
+
+// Input: head = [3,2,0,-4], pos = 1
+// Output: tail connects to node index 1
+// Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+// Example 2:
+
+// Input: head = [1,2], pos = 0
+// Output: tail connects to node index 0
+// Explanation: There is a cycle in the linked list, where tail connects to the first node.
+
+// Example 3:
+
+// Input: head = [1], pos = -1
+// Output: no cycle
+// Explanation: There is no cycle in the linked list.
+
+// Follow-up:
+// Can you solve it without using extra space?
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+  let fast = head;
+  let slow = head;
+
+  function findtheMeetPt() {
+    while (fast && fast.next) {
+      fast = fast.next.next;
+      slow = slow.next;
+      if (fast === slow) return true;
+    }
+    return false;
+  }
+
+  const meetNode = findtheMeetPt();
+  if (meetNode) {
+    while (slow !== head) {
+      head = head.next;
+      slow = slow.next;
+    }
+
+    return head;
+  }
+
+  return null;
+};
