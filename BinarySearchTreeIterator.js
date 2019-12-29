012f1b40bdabc30857e7ef24d6367130eac9b9ca
Kevin <kevinqiyf@hotmail.com>
2019-12-28 19:16:52 -0800

    add better space complexity solution

--- BinarySearchTreeIterator.js
+++ BinarySearchTreeIterator.js
@@ -19,20 +19,66 @@
  * @constructor
  * @param {TreeNode} root - root of the binary search tree
  */
-var BSTIterator = function(root) {
-  this.data = [];
-  this.find(root);
-};
+// var BSTIterator = function(root) {
+//   this.data = [];
+//   this.find(root);
+// };
+
+// /**
+//  * @this BSTIterator
+//  * @param {TreeNode} root
+//  */
+// BSTIterator.prototype.find = function(root) {
+//   if (root) {
+//     this.find(root.left);
+//     this.data.push(root.val);
+//     this.find(root.right);
+//   }
+// };
+
+// /**
+//  * @this BSTIterator
+//  * @returns {boolean} - whether we have a next smallest number
+//  */
+// BSTIterator.prototype.hasNext = function() {
+//   return this.data.length > 0;
+// };
+
+// /**
+//  * @this BSTIterator
+//  * @returns {number} - the next smallest number
+//  */
+// BSTIterator.prototype.next = function() {
+//   return this.data.shift();
+// };
 
 /**
- * @this BSTIterator
- * @param {TreeNode} root
+ * Your BSTIterator will be called like this:
+ * var i = new BSTIterator(root), a = [];
+ * while (i.hasNext()) a.push(i.next());
  */
-BSTIterator.prototype.find = function(root) {
-  if (root) {
-    this.find(root.left);
-    this.data.push(root.val);
-    this.find(root.right);
+
+/**
+ * Definition for binary tree
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @constructor
+ * @param {TreeNode} root - root of the binary search tree
+ */
+var BSTIterator = function(root) {
+  this.data = [];
+  this.leftmost_inorder(root);
+};
+
+BSTIterator.prototype.leftmost_inorder = function(root) {
+  while (root) {
+    this.data.push(root);
+    root = root.left;
   }
 };
 
@@ -49,7 +95,14 @@ BSTIterator.prototype.hasNext = function() {
  * @returns {number} - the next smallest number
  */
 BSTIterator.prototype.next = function() {
-  return this.data.shift();
+  let topmost_node = this.data.pop();
+  let right = topmost_node.right;
+
+  if (right) {
+    this.leftmost_inorder(right);
+  }
+
+  return topmost_node.val;
 };
 
 /**
