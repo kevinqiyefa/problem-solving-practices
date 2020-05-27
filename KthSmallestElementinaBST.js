3004ee86830cb63b90b041593583c0e0b647b8ad
Kevin <kevinqiyf@hotmail.com>
2020-05-27 10:39:33 -0700

    add more

---
+++ KthSmallestElementinaBST.js
@@ -0,0 +1,63 @@
+// 230. Kth Smallest Element in a BST
+
+// Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+
+// Example 1:
+
+// Input: root = [3,1,4,null,2], k = 1
+//    3
+//   / \
+//  1   4
+//   \
+//    2
+// Output: 1
+// Example 2:
+
+// Input: root = [5,3,6,2,4,null,null,1], k = 3
+//        5
+//       / \
+//      3   6
+//     / \
+//    2   4
+//   /
+//  1
+// Output: 3
+// Follow up:
+// What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
+
+// Constraints:
+
+// The number of elements of the BST is between 1 to 10^4.
+// You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function (root, k) {
+  let res = [0, null];
+
+  inorder(root, k, res);
+
+  return res[1];
+};
+
+function inorder(rt, k, res) {
+  if (!rt) return;
+
+  inorder(rt.left, k, res);
+  if (++res[0] === k) {
+    res[1] = rt.val;
+    return;
+  }
+  inorder(rt.right, k, res);
+}
