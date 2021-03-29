6981aef7d082f995b44ebf4cab2593a262ffd5ab
Kevin <kevinqiyf@hotmail.com>
2021-03-28 21:51:07 -0700

    add more

--- SpiralMatrixArray.js
+++ SpiralMatrixArray.js
@@ -16,47 +16,51 @@
 //     [10,  9,  8, 7]]
 
 function spiralMatrix(n) {
-  let matrix = [];
-  let value = 1;
-  for (let i = 0; i < n; i++) {
-    matrix.push([]);
-  }
+	let matrix = [];
+	let value = 1;
+	for (let i = 0; i < n; i++) {
+		matrix.push([]);
+	}
 
-  let start_row = 0,
-    start_col = 0,
-    end_row = n - 1,
-    end_col = n - 1;
+	let start_row = 0,
+		start_col = 0,
+		end_row = n - 1,
+		end_col = n - 1;
 
-  while (start_row <= end_row && start_col <= end_col) {
-    //top row
-    for (let i = start_col; i <= end_col; i++) {
-      matrix[start_row][i] = value;
-      value++;
-    }
-    start_row++;
+	while (start_row <= end_row && start_col <= end_col) {
+		//top row
+		for (let i = start_col; i <= end_col; i++) {
+			matrix[start_row][i] = value;
+			value++;
+		}
+		start_row++;
 
-    //right column
-    for (let i = start_row; i <= end_row; i++) {
-      matrix[i][end_col] = value;
-      value++;
-    }
-    end_col--;
+		//right column
+		for (let i = start_row; i <= end_row; i++) {
+			matrix[i][end_col] = value;
+			value++;
+		}
+		end_col--;
 
-    //bottom row
-    for (let i = end_col; i >= start_col; i--) {
-      matrix[end_row][i] = value;
-      value++;
-    }
-    end_row--;
+		//bottom row
+		if (start_row <= end_row) {
+			for (let i = end_col; i >= start_col; i--) {
+				matrix[end_row][i] = value;
+				value++;
+			}
+			end_row--;
+		}
 
-    //left column
-    for (let i = end_row; i >= start_row; i--) {
-      matrix[i][start_col] = value;
-      value++;
-    }
-    start_col++;
-  }
-  return matrix;
+		//left column
+		if (start_col <= end_col) {
+			for (let i = end_row; i >= start_row; i--) {
+				matrix[i][start_col] = value;
+				value++;
+			}
+			start_col++;
+		}
+	}
+	return matrix;
 }
 
 spiralMatrix(3);
